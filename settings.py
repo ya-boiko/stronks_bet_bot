a746bb15f12f34cd269e80e3d9ae494848f0520a
Vladislav Boiko <vladboiko1996@yandex.ru>
2023-03-15 22:22:30 +0300

    removes admins settings

--- settings.py
+++ settings.py
@@ -8,10 +8,4 @@ dotenv_path = Path(".env")
 load_dotenv(dotenv_path=dotenv_path)
 
 TOKEN = os.getenv("TOKEN")
-ADMIN_TOKEN = os.getenv("ADMIN_TOKEN")
 DB_NAME = os.getenv("DB_NAME")
-
-ADMINS = []
-for admin in os.getenv("ADMINS").split(";"):
-    if admin.strip():
-        ADMINS.append(admin.strip())
